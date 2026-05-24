9693f38ee8b49f1ca2492948d6a5be5a13191449
dicatop0001-maker <dicatop0001@gmail.com>
2026-05-24 14:51:42 -0300

    fix: circulo laranja estatico + fonte 6.5px para PROFISSIONAL DO BAIRRO completo

--- src/ProfissionaisBar.jsx
+++ src/ProfissionaisBar.jsx
@@ -338,7 +338,7 @@ function ProfissionaisBar({ userCity }) {
       <div className="pb-wrap">
         <div className="pb-fixed" onClick={() => setModal({ type: 'cadastro' })}>
             <div className="pb-circle cadastro">
-              <span style={{ fontSize: '7.5px', lineHeight: '1.25', fontWeight: '900', textAlign: 'center', display:'block', padding:'2px' }}>
+              <span style={{ fontSize: '6.5px', lineHeight: '1.3', fontWeight: '900', textAlign: 'center', display:'block', padding:'1px', letterSpacing:'-0.2px' }}>
                 PROFISSIONAL<br/>DO<br/>BAIRRO
               </span>
             </div>
@@ -370,6 +370,7 @@ function ProfissionaisBar({ userCity }) {
             )
           })}
         </div>
+          </div>
           </div>
       </div>
       {modal && (
